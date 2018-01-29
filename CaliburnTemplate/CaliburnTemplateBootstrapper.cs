e322ac3ffc82abc24c4e4e6257eef6b629ad2bee
Unknown <stutz-marco@gmx.ch>
2018-01-29 11:50:33 +0100

    Initial Calculations

---
+++ CaliburnTemplate/CaliburnTemplateBootstrapper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Autofac;
+using Caliburn.Micro.Autofac;
+using CaliburnTemplate.Models;
+using CaliburnTemplate.ViewModels;
+
+namespace CaliburnTemplate
+{
+    class CaliburnTemplateBootstrapper : AutofacBootstrapper<MainViewModel>
+    {
+        public CaliburnTemplateBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterType<PaddingService>().As<IPaddingService>();
+        }
+
+        protected override void ConfigureBootstrapper()
+        {
+            base.ConfigureBootstrapper();
+            EnforceNamespaceConvention = false;
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
---
+++ CaliburnTemplate/ViewModels/MainViewModel.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using CaliburnTemplate.Models;
+
+namespace CaliburnTemplate.ViewModels
+{
+    internal class MainViewModel : PropertyChangedBase
+    {
+        public MainViewModel(IPaddingService paddingService)
+        {
+            this.PaddingService = paddingService;
+        }
+        
+        public IPaddingService PaddingService { get; }
+
+        public string output;
+        public string firstInput;
+        public string sign;
+
+        public string Output
+        {
+            get => this.output;
+            set
+            {
+                output = value;
+                NotifyOfPropertyChange(nameof(Output));
+            }
+        }
+
+        public void Plus()
+        {
+            SetOperator("+");
+        }
+
+        public void Minus()
+        {
+            SetOperator("-");
+        }
+
+        public void Multiply()
+        {
+            SetOperator("*");
+        }
+
+        public void Divide()
+        {
+            SetOperator("/");
+        }
+
+        public void Equal()
+        {
+            double numOne = Convert.ToDouble(firstInput);
+            double numTwo = Convert.ToDouble(Output);
+            switch (sign)
+            {
+                case "+":
+                    Output = (numOne + numTwo).ToString();
+                    break;
+                case "-":
+                    Output = (numOne - numTwo).ToString();
+                    break;
+                case "*":
+                    Output = (numOne * numTwo).ToString();
+                    break;
+                case "/":
+                    Output = (numOne / numTwo).ToString();
+                    break;
+            }
+        }
+
+        public void SetOperator(string sign)
+        {
+            firstInput = Output;
+            this.sign = sign;
+            Output = "";
+        }
+
+        public void PrintZero()
+        {
+            Output += 0.ToString();
+        }
+
+        public void PrintOne()
+        {
+            Output += 1.ToString();
+        }
+
+        public void PrintTwo()
+        {
+            Output += 2.ToString();
+        }
+
+        public void PrintThree()
+        {
+            Output += 3.ToString();
+        }
+
+        public void PrintFour()
+        {
+            Output += 4.ToString();
+        }
+
+        public void PrintFive()
+        {
+            Output += 5.ToString();
+        }
+
+        public void PrintSix()
+        {
+            Output += 6.ToString();
+        }
+
+        public void PrintSeven()
+        {
+            Output += 7.ToString();
+        }
+
+        public void PrintEight()
+        {
+            Output += 8.ToString();
+        }
+
+        public void PrintNine()
+        {
+            Output += 9.ToString();
+        }
+    }
+}
